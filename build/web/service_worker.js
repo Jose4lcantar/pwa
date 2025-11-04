744d2667a5b8fa226d7d4495c09b098c61adff88
chema <alcantarchema261@gmail.com>
2025-11-04 10:49:04 -0600

    Add web build

---
+++ build/web/service_worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'valetflow-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.dart.js',
+  '/manifest.json',
+  '/icons/Icon-192.png',
+  '/icons/Icon-512.png',
+  '/icons/Icon-maskable-192.png',
+  '/icons/Icon-maskable-512.png',
+  // agrega aquí otros assets que quieras cachear
+];
+
+// Instalación: cacheamos archivos
+self.addEventListener('install', (event) => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activación: limpia caches viejas
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch: responde con cache o con la red
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
